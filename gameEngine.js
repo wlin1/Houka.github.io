ce4c1711df4d5ffcc78e0ba60e0c67315eaa9e06
Changxu Lu <cl795@cornell.edu>
2015-02-23 00:59:50 -0500

    restructuring in progess

---
+++ gameEngine.js
@@ -0,0 +1,23 @@
+//Player Class
+function GameObj(w, h){
+	//vars
+	this.width = w;
+	this.height = h;
+	this.facingRight = false;
+	//movement
+	this.left = false;
+	this.right = false;
+	this.up = false;
+	this.down = false;
+
+	//main functions
+	this.update = function(){
+
+	};
+	this.draw = function(context){
+
+	};
+	//helper functions
+	this.
+
+};
